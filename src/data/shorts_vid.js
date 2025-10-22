5691224819d9525fee7090363b9191bb24d1501f
이호준 <hjsj23430@naver.com>
2025-10-22 16:42:24 +0900

    add

--- src/data/shorts_vid.js
+++ src/data/shorts_vid.js
@@ -3,6 +3,12 @@ export const shorts = [
     { id: 2, src: '/assets/shorts_vid/Teto_Fruits.mp4', title: 'Teto Fruits', author: 'Teritory' },
     { id: 3, src: '/assets/shorts_vid/TetoPear.mp4', title: 'TetoPear', author: 'Miku' },
     { id: 4, src: '/assets/shorts_vid/Teto_World_of_the_Day.mp4', title: 'Teto World of the day!', author: 'Neru' },
-    { id: 5, src: '/assets/shorts_vid/TWotD.mp4', title: 'Teto World of the day!', author: 'Teto' }
+    { id: 5, src: '/assets/shorts_vid/TWotD.mp4', title: 'Teto World of the day!', author: 'Teto' },
+    { id: 6, src: '/assets/shorts_vid/Baguette.mp4', title: 'Baguette Man', author: 'Baguette' },
+    { id: 7, src: '/assets/shorts_vid/TetoTetoTeto.mp4', title: 'TetoTetoTetoTetoTetoTetoTetoTetoTetoTeto', author: 'Teto maker' },
+    { id: 8, src: '/assets/shorts_vid/Waffle_meme.mp4', title: 'Waffle Flip', author: 'Teto' },
+    { id: 9, src: '/assets/shorts_vid/Teto_La_AI.mp4', title: 'Teto La AI??!?!?!', author: 'LT.Linh:3' },
+    { id: 10, src: '/assets/shorts_vid/Life_of_Pear.mp4', title: 'Life of Pear', author: 'Pear' },
+    { id: 11, src: '/assets/shorts_vid/Hito_Mania.mp4', title: 'Hito Mania', author: 'Miku' }
   ]
   
